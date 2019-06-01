ee95c340460d141d10c92fd68403d41d128dd060
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-06-01 23:47:08 +0200

    Add possibility to set an offset for ready data from ring-buffer. This is totally necessary for reading mulitple pakets in the stream.

--- include/libKitsuneNetwork/messageringbuffer.h
+++ include/libKitsuneNetwork/messageringbuffer.h
@@ -24,18 +24,21 @@ struct MessageRingBuffer
 inline const uint8_t*
 getDataPointer(const MessageRingBuffer &recvBuffer,
                uint8_t* backupPointer,
-               const uint32_t size)
+               const uint32_t size,
+               const uint32_t offset=0)
 {
-    if(recvBuffer.readPosition + size  > recvBuffer.totalBufferSize)
+    const uint32_t startPosition = (recvBuffer.readPosition + offset) % recvBuffer.totalBufferSize;
+
+    if(startPosition + size  > recvBuffer.totalBufferSize)
     {
         // only copy data, when absolut necessary
-        const uint32_t firstPart = size - ((recvBuffer.readPosition + size) % recvBuffer.totalBufferSize);
-        mempcpy(backupPointer, &recvBuffer.data[recvBuffer.readPosition], firstPart);
+        const uint32_t firstPart = size - ((startPosition + size) % recvBuffer.totalBufferSize);
+        mempcpy(backupPointer, &recvBuffer.data[startPosition], firstPart);
         mempcpy(&backupPointer[firstPart], &recvBuffer.data[0], size - firstPart);
         return backupPointer;
     }
 
-    return &recvBuffer.data[recvBuffer.readPosition];
+    return &recvBuffer.data[startPosition];
 }
 
 }
