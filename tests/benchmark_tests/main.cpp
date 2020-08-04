efa666ee642eac14880e74c94ab37344875857db
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-08-04 23:15:48 +0200

    replace boost argument-parser by libKitsunemimiArgs

--- tests/benchmark_tests/main.cpp
+++ tests/benchmark_tests/main.cpp
@@ -7,81 +7,66 @@
  */
 
 #include <libKitsunemimiPersistence/logger/logger.h>
-#include <boost/program_options.hpp>
+#include <libKitsunemimiArgs/arg_parser.h>
 #include <test_session.h>
 
 using Kitsunemimi::Persistence::initConsoleLogger;
-namespace argParser = boost::program_options;
-
 
 int main(int argc, char *argv[])
 {
-    // define arg-parser
-    argParser::options_description desc("Allowed options");
-    desc.add_options()
-        (
-            "help,h", // -h and --help for help-text
-            "produce help message"
-        )
-        (
-            "address,a",
-            argParser::value<std::string>(),
-            "address to connect"
-        )
-        (
-            "port,p",
-            argParser::value<uint16_t>(),
-            "port where to listen"
-        )
-        (
-            "type,t",
-            argParser::value<std::string>(),
-            "type: tcp or uds"
-        )
-    ;
+    //initConsoleLogger(true);
+    Kitsunemimi::Args::ArgParser argParser;
 
-    // exec arg-parser
-    argParser::variables_map vm;
-    argParser::store(argParser::parse_command_line(argc, argv, desc), vm);
-    argParser::notify(vm);
+    argParser.registerString("address,a",
+                             "address to connect (Default: 127.0.0.1)");
+    argParser.registerInteger("port,p",
+                              "port where to listen (Default: 4321)");
+    argParser.registerString("socket,s",
+                             "type: tcp or uds (Default: tcp)");
+    argParser.registerString("transfer-type,t",
+                             "type of transfer: stream, standalone or request (Default: stream)");
+    argParser.registerInteger("package-size",
+                              "Test-package-size in byte(Default: 128 KiB)",
+                              true,
+                              true);
 
-    // help-arg
-    if(vm.count("help"))
-    {
-        std::cout << desc << std::endl;
-        return 0;
+    bool ret = argParser.parse(argc, argv);
+    if(ret == false) {
+        return 1;
     }
 
-    // init values
-    uint16_t port = 0;
-    std::string address = "";
-    std::string type = "";
+    uint16_t port = 4321;
+    std::string address = "127.0.0.1";
+    std::string socket = "tcp";
+    std::string transferType = "stream";
 
-    // get values from command line
-    if(vm.count("address")) {
-        address = vm["address"].as<std::string>();
+    if(argParser.wasSet("address")) {
+        address = argParser.getStringValues("address").at(0);
     }
-    if(vm.count("port")) {
-        port = vm["port"].as<uint16_t>();
+    if(argParser.wasSet("port")) {
+        port = static_cast<uint16_t>(argParser.getIntValues("port").at(0));
     }
-    if(vm.count("type")) {
-        type = vm["type"].as<std::string>();
+    if(argParser.wasSet("socket")) {
+        socket = argParser.getStringValues("socket").at(0);
+    }
+    if(argParser.wasSet("transfer-type")) {
+        transferType = argParser.getStringValues("transfer-type").at(0);
     }
 
     // debug-output
     std::cout<<"address: "<<address<<std::endl;
-    std::cout<<"port: "<<(int)port<<std::endl;
-    std::cout<<"type: "<<type<<std::endl;
+    std::cout<<"port: "<<static_cast<int>(port)<<std::endl;
+    std::cout<<"type: "<<socket<<std::endl;
 
     // check type
-    if(type != "tcp"
-            && type != "uds")
+    if(socket != "tcp"
+            && socket != "uds")
     {
-        std::cout<<"ERROR: type \""<<type<<"\" is unknown. Choose \"tcp\" or \"uds\"."<<std::endl;;
+        std::cout<<"ERROR: type \""<<socket<<"\" is unknown. Choose \"tcp\" or \"uds\"."<<std::endl;
         exit(1);
     }
 
     // run test
-    Kitsunemimi::Network::TestSession testSession(address, port, type);
+    Kitsunemimi::Network::TestSession testSession(address, port, socket);
     testSession.runTest();
 }
